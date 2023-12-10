e65ee51082bc6522c8c4533f40888e7313f8ed97
vinicius.scarvalho@al.edu.infnet.com.br <vinicius.scarvalho@al.edu.infnet.com.br>
2023-12-10 14:51:58 -0300

    validações :)

--- projeto_dr3-backend/src/main/java/br/edu/infnet/MainApp.java
+++ projeto_dr3-backend/src/main/java/br/edu/infnet/MainApp.java
@@ -1,11 +1,15 @@
 package br.edu.infnet;
 
+import br.edu.infnet.config.DatabaseConfig;
 import br.edu.infnet.controller.UsuarioController;
 
 import static spark.Spark.awaitInitialization;
 
 public class MainApp {
     public static void main(String[] args) {
+        // Inicializa o banco de dados
+        DatabaseConfig.inicializarBancoDeDados();
+
         UsuarioController usuarioController = new UsuarioController();
         usuarioController.respostasRequisicoes();
         awaitInitialization();
